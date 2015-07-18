a5658905e78864f7d1a4cdb8b02aa37ba2cfd383
Tom Hunger <tehunger@gmail.com>
2015-07-18 17:53:15 +0100

    Parse email_queue table.

--- mail-queue.nix
+++ mail-queue.nix
@@ -1,10 +1,15 @@
-{ mkDerivation, base, bytestring, mime-mail, smtp-mail, stdenv }:
+{ mkDerivation, aeson, base, bytestring, graceful, mime-mail
+, postgresql-simple, safe, smtp-mail, stdenv, time
+}:
 mkDerivation {
   pname = "mail-queue";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  buildDepends = [ base bytestring mime-mail smtp-mail ];
+  buildDepends = [
+    aeson base bytestring graceful mime-mail postgresql-simple safe
+    smtp-mail time
+  ];
   license = stdenv.lib.licenses.gpl3;
 }
--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,65 @@
 {-# LANGUAGE OverloadedStrings #-}
+-- This module selects the email_queue table every 500ms and sends all
+-- the emails that still need sending.
 
-import Network.Mail.SMTP (sendMail)
+-- TODO
+-- * use graceful package
+
+import           Control.Concurrent (threadDelay)
+import           Control.Monad (forever)
+import           Data.Aeson (decode)
+import qualified Data.ByteString.Lazy.Char8 as BS
+import           Data.Time.Clock (UTCTime)
+import qualified Database.PostgreSQL.Simple as S
 import qualified Network.Mail.Mime as M
+import           Network.Mail.SMTP (sendMail)
+import           Safe (readMay)
+
+data EmailType = INVITE
+               | VERIFY_ADDRESS
+               | NEW_DEVICE
+               | ISSUE_REPORTED
+               | ONBOARD_FIRST_SECRET
+               | ERROR String deriving Show
+
+instance Read EmailType where
+    readsPrec _ "new_user_invitation" = [(INVITE, "")]
+    readsPrec _ "address_verification" = [(VERIFY_ADDRESS, "")]
+    readsPrec _ "new_device_login" = [(NEW_DEVICE, "")]
+    readsPrec _ "issue_reported" = [(ISSUE_REPORTED, "")]
+    readsPrec _ "onboard-first-secret" = [(ONBOARD_FIRST_SECRET, "")]
+    readsPrec _ r = [(ERROR r, r)]
+
+renderMail :: M.Address -> EmailType -> IO M.Mail
+renderMail to INVITE =
+    M.simpleMail
+    to
+    (M.Address (Just "Passopolis") "team@passopolis.com")
+    "invite"
+    "invite"
+    "invite"
+    []
+
+parseMsg :: (String, Maybe String) -> Either String (EmailType, [Maybe String])
+parseMsg (type_, args) =
+    case readMay type_ of
+        Just ep -> case decodeArgs args of
+          Just argsOK -> Right (ep, argsOK)
+          Nothing -> Left "Could not decode json args"
+        Nothing -> Left ("Unknown message type " ++ type_)
+  where
+    decodeArgs :: Maybe String -> Maybe [Maybe String]
+    decodeArgs Nothing = Just []
+    decodeArgs (Just s) = decode (BS.pack s) :: Maybe [Maybe String]
+
+waitForEmail :: IO ()
+waitForEmail = forever $ do
+    conn <- S.connectPostgreSQL "host=127.0.0.1 dbname='mitro'"
+    r <- S.query_ conn "select type_string, arg_string from email_queue" :: IO [(String, Maybe String)]
+
+    print $ map parseMsg r
+    threadDelay (1 * 1000 * 1000)
 
 main = do
-    m <- M.simpleMail
-       (M.Address (Just "Tom") "tom@wearewizards.io")
-       (M.Address (Just "Passopolis") "team@passopolis.com")
-       "hello"
-       "body"
-       ""
-       []
-
-    sendMail "127.0.0.1" m
+    waitForEmail
+    -- sendMail "127.0.0.1" m
